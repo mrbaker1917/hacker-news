81157908f04ebcb0f74e8416d9023c1bc8c26ba6
Mark R Baker <mrbaker1917@gmail.com>
2021-10-06 13:02:17 -0700

    initial setup with webpack

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hacker-news
  UI project
+This is a project to improve my React skills by creating an app to view hacker news items.
---
+++ app/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react";
+import "./index.css";
+
+// Component
+// State
+// Lifecycle
+// UI
+
+class App extends React.Component {
+  render() {
+    return <div>Hacker News</div>
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("app")
+)
